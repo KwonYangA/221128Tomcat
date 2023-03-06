a4d4f85158304761a03eeb67d2ed88710adea1bc
KwonYangA <yanga3867@gmail.com>
2023-03-06 17:43:56 +0900

    230306Board

--- chat221228/src/main/java/com/pojo/step3/Controller3.java
+++ chat221228/src/main/java/com/pojo/step3/Controller3.java
@@ -1,12 +1,18 @@
 package com.pojo.step3;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+//javascript기반의 UI API
+//바닐라 스트립트
+//리액트 = 바닐라스클립트+ES6(주요이슈-spread, module, arrowfunction),ES7
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Controller3 {
+ public Object jsonBoardList(HttpServletRequest req, HttpServletResponse res);
  public Object boardList(HttpServletRequest req, HttpServletResponse res);
  public Object boardDetail(HttpServletRequest req, HttpServletResponse res);
- public Object boardInsert(HttpServletRequest req, HttpServletResponse res);
- public Object boardUpdate(HttpServletRequest req, HttpServletResponse res);
- public Object boardDelete(HttpServletRequest req, HttpServletResponse res);
+ public Object boardInsert(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException;
+ public Object boardUpdate(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException;
+ public Object boardDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException;
 }
--- chat221228/src/main/java/com/pojo/step3/HandlerMapping.java
+++ chat221228/src/main/java/com/pojo/step3/HandlerMapping.java
@@ -7,22 +7,83 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 public class HandlerMapping {
- /*******************************************************************************
- @param	upmu[](upmu[0]-업무명[폴더명], upmu[1]-메소드명, 기능명, 페이지이름, 분기)
- @param request -1-1, 1-2와는 다르게 인터페이스를 implements하지 않는다
- @param response
- 질문? 어디서 받아오지?
- @return Object
- *******************************************************************************/	
+	/*******************************************************************************
+	 * @param upmu[](upmu[0]-업무명[폴더명], upmu[1]-메소드명, 기능명, 페이지이름, 분기)
+	 * @param request                  -1-1, 1-2와는 다르게 인터페이스를 implements하지 않는다
+	 * @param response                 질문? 어디서 받아오지?
+	 * @return Object
+	 *******************************************************************************/
 	static Logger logger = Logger.getLogger(Board3Controller.class);
-	
-	public static Object getController(String[] upmu, HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException{
-	logger.info(upmu[0]+","+upmu[1]);
-	Controller3 controller = null;
-	String path = null;
-	ModelAndView mav = null;
-	
-	return null;
-	}
-	
+
+	public static Object getController(String[] upmu, HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		logger.info(upmu[0] + "," + upmu[1]);
+		Controller3 controller = null;
+		String path = null;
+		Object obj = null;
+		ModelAndView mav = null;
+		if ("board3".equals(upmu[0])) {
+			controller = new Board3Controller();
+			// 게시글 전체 목록
+			if ("boardList".equals(upmu[1])) {// html화면 출력이 나감- text/html
+				obj = controller.boardList(req, res);
+				// 리턴타입이 ModelAndView
+				if (obj instanceof ModelAndView) {
+					return (ModelAndView) obj;
+				}
+				// 리턴타입이 ModelAndView
+				else if (obj instanceof String) {
+					return (String) obj;
+				}
+			} else if ("jsonBoardList".equals(upmu[1])) {// json포맷으로 나감 - application/json
+				obj = controller.jsonBoardList(req, res);
+				// 리턴타입이 ModelAndView
+				if (obj instanceof ModelAndView) {
+					return (ModelAndView) obj;
+				}
+				// 리턴타입이 ModelAndView
+				else if (obj instanceof String) {
+					return (String) obj;
+				}
+			} else if ("boardDetail".equals(upmu[1])) {// json포맷으로 나감 - application/json
+					obj = controller.boardDetail(req, res);
+					// 리턴타입이 ModelAndView
+					if (obj instanceof ModelAndView) {
+						return (ModelAndView) obj;
+					}
+					// 리턴타입이 ModelAndView
+					else if (obj instanceof String) {
+						return (String) obj;
+					}
+			} else if ("boardInsert".equals(upmu[1])) {// 글입력 - 새글쓰기와 댓글쓰기
+				obj = controller.boardInsert(req, res);
+				logger.info("boardInsert==> boolean :" + obj instanceof String);
+				// 리턴타입이 ModelAndView
+				if (obj instanceof String) {
+					return (String) obj;
+				}
+			} else if ("boardUpdate".equals(upmu[1])) {// 글수정- 첨부파일 유무 고려하기
+				obj = controller.boardUpdate(req, res);
+				if (obj instanceof String) {
+					return (String) obj;
+				}
+			} else if ("boardDelete".equals(upmu[1])) {// 글삭제 -
+				obj = controller.boardDelete(req, res);
+				if (obj instanceof String) {
+					return (String) obj;
+				}
+			}
+		} // end of 게시판 구현
+			// 회원관리나 인증관리 등등 다른 기능 구현
+		else if ("auth".equals(upmu[0])) {
+
+		} else if ("member".equals(upmu[0])) {
+
+		} // 주문관리
+		else if ("order".equals(upmu[0])) {
+
+		}
+		return obj;
+		}
 }
+
